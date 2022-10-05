fec33d7d56ca6ed122197e884a37cde80cac0b4c
TMG <66220144+TMG8047KG@users.noreply.github.com>
2022-10-05 19:18:10 +0300

    1.0.3-alpha

--- src/main/java/me/rosekingdom/rosekingdom/Commands/Coordinates_Share.java
+++ src/main/java/me/rosekingdom/rosekingdom/Commands/Coordinates_Share.java
@@ -9,27 +9,62 @@ import org.bukkit.entity.Player;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.bukkit.Bukkit.getLogger;
+
 public class Coordinates_Share implements TabExecutor {
 
     public int x;
     public int y;
     public int z;
-
+    private boolean nonPlayer;
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(sender instanceof Player){
-            Player p = (Player) sender;
+        if(sender instanceof Player p){
 
             if(args.length > 0){
-                Player target = Bukkit.getPlayer(args[0]);
+                Player target;
                 x = p.getLocation().getBlockX();
                 y = p.getLocation().getBlockY();
                 z = p.getLocation().getBlockZ();
 
-                target.sendMessage("§e" + p.getName() + " send you his coordinates §6X:§r " + x + "§6 Y:§r " + y + "§6 Z:§r " + z);
-                p.sendMessage("§eYou send your coordinates to " + target.getName());
+                Player[] players = new Player[Bukkit.getServer().getOnlinePlayers().size()];
+                Bukkit.getServer().getOnlinePlayers().toArray(players);
+
+                try{
+                    if(args[0].contains("all")){
+                        for (Player player : players) {
+                            if (!(player.getName().equals(p.getName()))) {
+                                player.sendMessage("§e" + p.getName() + " send his (current) coordinates to everyone §6X:§r " + x + "§6 Y:§r " + y + "§6 Z:§r " + z);
+                            }
+                        }
+                    }else{
+
+                        for(Player player : players){
+                            if(args[0].equals(player.getName())){
+                                nonPlayer = false;
+                                break;
+                            }else {
+                                nonPlayer = true;
+                            }
+                        }
+                        if(nonPlayer){
+                            p.sendMessage("§cPlayer not found!");
+                            return true;
+                        }
+                        target = Bukkit.getPlayer(args[0]);
+                        if(target.getName().equals(p.getName())){
+                            p.sendMessage("§cYou can't send your coordinates to yourself!");
+                            return true;
+                        }
+
+                        target.sendMessage("§e" + p.getName() + " send his (current) coordinates to you§6X:§r " + x + "§6 Y:§r " + y + "§6 Z:§r " + z);
+                        p.sendMessage("§eYou send your coordinates to " + target.getName());
+                    }
+                }catch (Exception e){
+                    getLogger().warning("Error: " + e);
+                }
             }else{
-                p.sendMessage("§cSelect a player");
+                p.sendMessage("§cSelect someone by typing their username!");
             }
         }
         return true;
@@ -38,11 +73,15 @@ public class Coordinates_Share implements TabExecutor {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
         if (args.length == 1) {
+            Player player = (Player) sender;
             List<String> playerNames = new ArrayList<>();
             Player[] players = new Player[Bukkit.getServer().getOnlinePlayers().size()];
             Bukkit.getServer().getOnlinePlayers().toArray(players);
-            for (int i = 0; i < players.length; i++) {
-                playerNames.add(players[i].getName());
+            playerNames.add("all");
+            for (Player value : players) {
+                if (!(value.getName().equals(player.getName()))) {
+                    playerNames.add(value.getName());
+                }
             }
             return playerNames;
         }
--- src/main/java/me/rosekingdom/rosekingdom/RoseKingdom.java
+++ src/main/java/me/rosekingdom/rosekingdom/RoseKingdom.java
@@ -2,31 +2,28 @@ package me.rosekingdom.rosekingdom;
 
 import me.rosekingdom.rosekingdom.Commands.Coordinates_Share;
 import me.rosekingdom.rosekingdom.Commands.SpawnEntity;
+import me.rosekingdom.rosekingdom.Listeners.*;
 import me.rosekingdom.rosekingdom.Materials.Items.Bucket_Hats;
 import me.rosekingdom.rosekingdom.Materials.Items.Mushroom_Hats;
-import me.rosekingdom.rosekingdom.Listeners.JoinLeaveListener;
 import me.rosekingdom.rosekingdom.Materials.Items.Medals;
-import me.rosekingdom.rosekingdom.Listeners.Riding_Player_Listener;
-import me.rosekingdom.rosekingdom.Listeners.Unplaceable_Hats;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
 public final class RoseKingdom extends JavaPlugin {
-
     @Override
     public void onEnable() {
         // Plugin startup logic
         getLogger().info("[RoseKingdom] Loaded!");
         getServer().getPluginManager().registerEvents(new JoinLeaveListener(), this);
         getServer().getPluginManager().registerEvents(new Unplaceable_Hats(), this);
-        getServer().getPluginManager().registerEvents(new Riding_Player_Listener(), this);
-        //getServer().getPluginManager().registerEvents(new Sign_Rewrite(), this);
+//        getServer().getPluginManager().registerEvents(new Riding_Player_Listener(), this);
+//        getServer().getPluginManager().registerEvents(new Chair(), this);
+//        getServer().getPluginManager().registerEvents(new Sign_Rewrite(), this);
 
         Medals.init();
         Bucket_Hats.init();
         Mushroom_Hats.init();
         Commands();
-        //new CommandManager(this);
     }
 
     public void Commands(){
